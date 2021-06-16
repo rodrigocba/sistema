d3be4b0c34d68d3ad41d8d432dc0f9b05402ef89
Rodri <rodrigocba@gmail.com>
2021-06-16 18:19:21 -0300

    comentario33

--- src/main/java/com/birra/sistema/SistemaApplication.java
+++ src/main/java/com/birra/sistema/SistemaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemaApplication {
 
 	public static void main(String[] args) {
-		//comentario
+		//comentario 333
 		SpringApplication.run(SistemaApplication.class, args);
 	}
 
